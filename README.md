cf35c762da46cb919ec06124143c7156d401cfe5
Paul <2paulclarke@gmail.com>
2022-04-26 17:17:00 +1200

    test 2

---
+++ README.md
@@ -0,0 +1,4 @@
+# Masonry - columnWidth
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/clarkepaul/pen/RwxByKo](https://codepen.io/clarkepaul/pen/RwxByKo).
+
---
+++ script.js
@@ -0,0 +1,23 @@
+// external js: masonry.pkgd.js
+
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 315
+});
+
+// Tabs
+// Show the first tab and hide the rest
+$('#tabs-nav li:first-child').addClass('active');
+$('.tab-content').hide();
+$('.tab-content:first').show();
+
+// Click function
+$('#tabs-nav li').click(function(){
+  $('#tabs-nav li').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-content').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
